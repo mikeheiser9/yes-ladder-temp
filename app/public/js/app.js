11e5a455c313c80d3b304c39d4a07114602087d0
mikeheiser9 <mikeheiser9@gmail.com>
2018-09-22 13:46:28 -0700

    added files

---
+++ app/public/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('#ladder-container').html("<div class='row ladder><h1 class='fill-head'>Advertorial Goes Here</h1></div>");
+    
+    $('#ladder-container').html("<div class='row ladder'><button type='button' class='btn btn-xl solidButtons' id='start-survey-btn' href='#startSurvey' data-toggle='modal'>Start Survey</button></div>");
+
+});
+
