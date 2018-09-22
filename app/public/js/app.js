a020ef8b580610354a2ff35143d9e3e3f5b689b0
mikeheiser9 <mikeheiser9@gmail.com>
2018-09-22 14:31:26 -0700

    first question framework

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-
-    $('#ladder-container').html("<div class='row ladder><h1 class='fill-head'>Advertorial Goes Here</h1></div>");
     
-    $('#ladder-container').html("<div class='row ladder'><button type='button' class='btn btn-xl solidButtons' id='start-survey-btn' href='#startSurvey' data-toggle='modal'>Start Survey</button></div>");
+    $('#ladder-container').html("<div class='row top-row'><h1 class='fill-head'>Advertorial Goes Here</h1></div><div class='row ladder'><button type='button' class='btn btn-xl solidButtons' id='start-survey-btn' href='#startSurvey' data-toggle='modal'>Start Survey</button></div>");
 
+    $("#survey-holder").html("<div class='row survey-question'><p>Carson Wentz has a huge massive penis?</p></div><div class='row input-row'><form action='POST'><input type='radio' name='selection' data-name='true'>True</input><br><br><input type='radio' name='selection' data-name='false'>False</input><br><br><input type='submit'></form></div>")
 });
 
